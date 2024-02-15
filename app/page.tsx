c56184bbcbc9af36473ffd456476c4e1773ce28c
니꼬 <nico@nomadcoders.co>
2024-02-15 18:53:08 +0900

    1.11 Directives

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 export default function Home() {
   return (
-    <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
+    <main className="bg-red-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
       <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-4">
-        <button className="w-full bg-black h-10 text-white rounded-sexy-name mt-tomato">
-          Submit
-        </button>
+        <input />
+        <a href="#">aaaa</a>
+        <button className="btn">Submit</button>
       </div>
     </main>
   );
