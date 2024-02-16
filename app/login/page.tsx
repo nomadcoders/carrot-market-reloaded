ee553993c95b85edfc3341ecdc96d137c050d1ff
니꼬 <nico@nomadcoders.co>
2024-02-16 18:06:53 +0900

    2.6 useFormStatus

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,8 +5,8 @@ import SocialLogin from "@/components/social-login";
 export default function LogIn() {
   async function handleForm(formData: FormData) {
     "use server";
-    console.log(formData.get("email"), formData.get("password"));
-    console.log("i run in the server baby!");
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    console.log("logged in!");
   }
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
@@ -29,7 +29,7 @@ export default function LogIn() {
           required
           errors={[]}
         />
-        <FormButton loading={false} text="Log in" />
+        <FormButton text="Log in" />
       </form>
       <SocialLogin />
     </div>
--- components/form-btn.tsx
+++ components/form-btn.tsx
@@ -1,15 +1,19 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+
 interface FormButtonProps {
-  loading: boolean;
   text: string;
 }
 
-export default function FormButton({ loading, text }: FormButtonProps) {
+export default function FormButton({ text }: FormButtonProps) {
+  const { pending } = useFormStatus();
   return (
     <button
-      disabled={loading}
+      disabled={pending}
       className="primary-btn h-10 disabled:bg-neutral-400  disabled:text-neutral-300 disabled:cursor-not-allowed"
     >
-      {loading ? "로딩 중" : text}
+      {pending ? "로딩 중" : text}
     </button>
   );
 }
