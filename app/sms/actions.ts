6d8084e84c775d630bdbab1ef62a39e5ceeadb5e
니꼬 <nico@nomadcoders.co>
2024-02-22 19:54:18 +0900

    4.7 Coerce

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,3 +1,13 @@
 "use server";
 
-export async function smsVerification(prevState: any, formData: FormData) {}
+import { z } from "zod";
+import validator from "validator";
+
+const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+
+const tokenSchema = z.coerce.number().min(100000).max(999999);
+
+export async function smsLogIn(prevState: any, formData: FormData) {
+  console.log(formData.get("token"));
+  console.log(tokenSchema.parse(formData.get("token")));
+}
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -3,10 +3,10 @@
 import Button from "@/components/button";
 import Input from "@/components/input";
 import { useFormState } from "react-dom";
-import { smsVerification } from "./actions";
+import { smsLogIn } from "./actions";
 
 export default function SMSLogin() {
-  const [state, dispatch] = useFormState(smsVerification, null);
+  const [state, dispatch] = useFormState(smsLogIn, null);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
@@ -20,6 +20,8 @@ export default function SMSLogin() {
           type="number"
           placeholder="Verification code"
           required
+          min={100000}
+          max={999999}
         />
         <Button text="Verify" />
       </form>
