25f5329c7b5f8f5001a0914763445d7dabf1216c
니꼬 <nico@nomadcoders.co>
2024-02-29 17:58:02 +0900

    6.2 Password Hashing

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,4 +1,5 @@
 "use server";
+import bcrypt from "bcrypt";
 import {
   PASSWORD_MIN_LENGTH,
   PASSWORD_REGEX,
@@ -86,8 +87,17 @@ export async function createAccount(prevState: any, formData: FormData) {
   if (!result.success) {
     return result.error.flatten();
   } else {
-    // hash password
-    // save the user to db
+    const hashedPassword = await bcrypt.hash(result.data.password, 12);
+    const user = await db.user.create({
+      data: {
+        username: result.data.username,
+        email: result.data.email,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+      },
+    });
     // log the user in
     // redirect "/home"
   }
