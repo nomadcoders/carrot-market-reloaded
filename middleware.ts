5bf2817a3c70c18b790e06ba13f77b186ded61fa
니꼬 <nico@nomadcoders.co>
2024-03-01 18:19:16 +0900

    6.10 Matcher

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,9 @@
-import { NextRequest, NextResponse } from "next/server";
-import getSession from "./lib/session";
+import { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const session = await getSession();
-  console.log(session);
-  if (request.nextUrl.pathname === "/profile") {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+  console.log("hello");
 }
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
