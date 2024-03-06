6e9c08596f5cc32c36780eaeafe51070fc2fe931
니꼬 <nico@nomadcoders.co>
2024-03-06 19:11:50 +0900

    6.13 Recap

--- middleware.ts
+++ middleware.ts
@@ -27,5 +27,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
 };
