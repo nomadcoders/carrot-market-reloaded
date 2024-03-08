704aee29d1b230e321de05d96cb4a349bf771eb4
니꼬 <nico@nomadcoders.co>
2024-03-08 13:08:36 +0900

    5.4 Relationships

--- lib/db.ts
+++ lib/db.ts
@@ -2,4 +2,17 @@ import { PrismaClient } from "@prisma/client";
 
 const db = new PrismaClient();
 
+async function test() {
+  const token = await db.sMSToken.findUnique({
+    where: {
+      id: 1,
+    },
+    include: {
+      user: true,
+    },
+  });
+  console.log(token);
+}
+test();
+
 export default db;
