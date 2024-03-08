8566a84b272e1f24b149e5953fa276654b87a801
니꼬 <nico@nomadcoders.co>
2024-03-08 19:02:52 +0900

    7.2 Access Token

---
+++ app/github/complete/route.ts
@@ -0,0 +1,28 @@
+import { notFound } from "next/navigation";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const code = request.nextUrl.searchParams.get("code");
+  if (!code) {
+    return notFound();
+  }
+  const accessTokenParams = new URLSearchParams({
+    client_id: process.env.GITHUB_CLIENT_ID!,
+    client_secret: process.env.GITHUB_CLIENT_SECRET!,
+    code,
+  }).toString();
+  const accessTokenURL = `https://github.com/login/oauth/access_token?${accessTokenParams}`;
+  const accessTokenResponse = await fetch(accessTokenURL, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  const accessTokenData = await accessTokenResponse.json();
+  if ("error" in accessTokenData) {
+    return new Response(null, {
+      status: 400,
+    });
+  }
+  return Response.json({ accessTokenData });
+}
