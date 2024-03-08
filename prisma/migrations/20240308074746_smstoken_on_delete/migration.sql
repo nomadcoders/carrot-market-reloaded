d77f5426b65f2dacae5702c130646f828bae667f
니꼬 <nico@nomadcoders.co>
2024-03-08 16:51:28 +0900

    5.5 onDelete

---
+++ prisma/migrations/20240308074746_smstoken_on_delete/migration.sql
@@ -0,0 +1,16 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_SMSToken" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "token" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    "userId" INTEGER NOT NULL,
+    CONSTRAINT "SMSToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_SMSToken" ("created_at", "id", "token", "updated_at", "userId") SELECT "created_at", "id", "token", "updated_at", "userId" FROM "SMSToken";
+DROP TABLE "SMSToken";
+ALTER TABLE "new_SMSToken" RENAME TO "SMSToken";
+CREATE UNIQUE INDEX "SMSToken_token_key" ON "SMSToken"("token");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
