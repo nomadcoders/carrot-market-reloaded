402e6f24380ef097ea34ea1c9e235de473641417
니꼬 <nico@nomadcoders.co>
2024-03-08 18:21:13 +0900

    7.1 Github Authentication

---
+++ app/github/start/route.ts
@@ -0,0 +1,11 @@
+export function GET() {
+  const baseURL = "https://github.com/login/oauth/authorize";
+  const params = {
+    client_id: process.env.GITHUB_CLIENT_ID!,
+    scope: "read:user,user:email",
+    allow_signup: "true",
+  };
+  const formattedParams = new URLSearchParams(params).toString();
+  const finalUrl = `${baseURL}?${formattedParams}`;
+  return Response.redirect(finalUrl);
+}
