c78400e5d8f3ae0a01b7f8918ac39453c5bcc8aa
니꼬 <nico@nomadcoders.co>
2024-03-08 18:20:09 +0900

    5.6 Recap

--- lib/db.ts
+++ lib/db.ts
@@ -2,17 +2,4 @@ import { PrismaClient } from "@prisma/client";
 
 const db = new PrismaClient();
 
-async function test() {
-  const token = await db.sMSToken.findUnique({
-    where: {
-      id: 1,
-    },
-    include: {
-      user: true,
-    },
-  });
-  console.log(token);
-}
-test();
-
 export default db;
