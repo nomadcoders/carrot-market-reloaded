bae1424de1e03841879bf5483a09a081f8704be3
니꼬 <nico@nomadcoders.co>
2024-03-20 19:15:34 +0900

    10.7 Modal UI

--- app/(tabs)/home/@modal/(...)products/[id]/page.tsx
+++ app/(tabs)/home/@modal/(...)products/[id]/page.tsx
@@ -1,3 +1,26 @@
-export default function Modal() {
-  return <h1>Im modal</h1>;
+"use client";
+
+import { PhotoIcon, XMarkIcon } from "@heroicons/react/24/solid";
+import { useRouter } from "next/navigation";
+
+export default function Modal({ params }: { params: { id: string } }) {
+  const router = useRouter();
+  const onCloseClick = () => {
+    router.back();
+  };
+  return (
+    <div className="absolute w-full h-full z-50 flex items-center justify-center bg-black bg-opacity-60 left-0 top-0">
+      <button
+        onClick={onCloseClick}
+        className="absolute right-5 top-5 text-neutral-200"
+      >
+        <XMarkIcon className="size-10" />
+      </button>
+      <div className="max-w-screen-sm h-1/2  flex justify-center w-full">
+        <div className="aspect-square  bg-neutral-700 text-neutral-200  rounded-md flex justify-center items-center">
+          <PhotoIcon className="h-28" />
+        </div>
+      </div>
+    </div>
+  );
 }
