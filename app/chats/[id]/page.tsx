5635989a7e485f07f0874315d2651b09e1713d57
니꼬 <nico@nomadcoders.co>
2024-03-27 16:19:19 +0900

    13.2 Chat Room

---
+++ app/chats/[id]/page.tsx
@@ -0,0 +1,32 @@
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { notFound } from "next/navigation";
+
+async function getRoom(id: string) {
+  const room = await db.chatRoom.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      users: {
+        select: { id: true },
+      },
+    },
+  });
+  if (room) {
+    const session = await getSession();
+    const canSee = Boolean(room.users.find((user) => user.id === session.id!));
+    if (!canSee) {
+      return null;
+    }
+  }
+  return room;
+}
+
+export default async function ChatRoom({ params }: { params: { id: string } }) {
+  const room = await getRoom(params.id);
+  if (!room) {
+    return notFound();
+  }
+  return <h1>chat!</h1>;
+}
--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -2,13 +2,13 @@ import db from "@/lib/db";
 import { formatToWon } from "@/lib/utils";
 import { UserIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
-import Link from "next/link";
-import { notFound } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 import {
   unstable_cache as nextCache,
   revalidatePath,
   revalidateTag,
 } from "next/cache";
+import getSession from "@/lib/session";
 
 async function getIsOwner(userId: number) {
   // const session = await getSession();
@@ -80,6 +80,28 @@ export default async function ProductDetail({
     "use server";
     revalidateTag("xxxx");
   };
+  const createChatRoom = async () => {
+    "use server";
+    const session = await getSession();
+    const room = await db.chatRoom.create({
+      data: {
+        users: {
+          connect: [
+            {
+              id: product.userId,
+            },
+            {
+              id: session.id,
+            },
+          ],
+        },
+      },
+      select: {
+        id: true,
+      },
+    });
+    redirect(`/chats/${room.id}`);
+  };
   return (
     <div className="pb-40">
       <div className="relative aspect-square">
@@ -122,12 +144,11 @@ export default async function ProductDetail({
             </button>
           </form>
         ) : null}
-        <Link
-          className="bg-orange-500 px-5 py-2.5 rounded-md text-white font-semibold"
-          href={``}
-        >
-          채팅하기
-        </Link>
+        <form action={createChatRoom}>
+          <button className="bg-orange-500 px-5 py-2.5 rounded-md text-white font-semibold">
+            채팅하기
+          </button>
+        </form>
       </div>
     </div>
   );
