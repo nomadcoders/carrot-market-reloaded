fd3de64a1a6268379c1475c080a8cf4684035932
니꼬 <nico@nomadcoders.co>
2024-03-27 17:58:46 +0900

    13.3 Messages

--- app/chats/[id]/page.tsx
+++ app/chats/[id]/page.tsx
@@ -1,5 +1,7 @@
+import ChatMessagesList from "@/components/chat-messages-list";
 import db from "@/lib/db";
 import getSession from "@/lib/session";
+import { Prisma } from "@prisma/client";
 import { notFound } from "next/navigation";
 
 async function getRoom(id: string) {
@@ -23,10 +25,37 @@ async function getRoom(id: string) {
   return room;
 }
 
+async function getMessages(chatRoomId: string) {
+  const messages = await db.message.findMany({
+    where: {
+      chatRoomId,
+    },
+    select: {
+      id: true,
+      payload: true,
+      created_at: true,
+      userId: true,
+      user: {
+        select: {
+          avatar: true,
+          username: true,
+        },
+      },
+    },
+  });
+  return messages;
+}
+
+export type InitialChatMessages = Prisma.PromiseReturnType<typeof getMessages>;
+
 export default async function ChatRoom({ params }: { params: { id: string } }) {
   const room = await getRoom(params.id);
   if (!room) {
     return notFound();
   }
-  return <h1>chat!</h1>;
+  const initialMessages = await getMessages(params.id);
+  const session = await getSession();
+  return (
+    <ChatMessagesList userId={session.id!} initialMessages={initialMessages} />
+  );
 }
---
+++ components/chat-messages-list.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { InitialChatMessages } from "@/app/chats/[id]/page";
+import { formatToTimeAgo } from "@/lib/utils";
+import { ArrowUpCircleIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+import { useRef, useState } from "react";
+
+interface ChatMessageListProps {
+  initialMessages: InitialChatMessages;
+  userId: number;
+}
+export default function ChatMessagesList({
+  initialMessages,
+  userId,
+}: ChatMessageListProps) {
+  const [messages, setMessages] = useState(initialMessages);
+  const [message, setMessage] = useState("");
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value },
+    } = event;
+    setMessage(value);
+  };
+  const onSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    alert(message);
+    setMessage("");
+  };
+  return (
+    <div className="p-5 flex flex-col gap-5 min-h-screen justify-end">
+      {messages.map((message) => (
+        <div
+          key={message.id}
+          className={`flex gap-2 items-start ${
+            message.userId === userId ? "justify-end" : ""
+          }`}
+        >
+          {message.userId === userId ? null : (
+            <Image
+              src={message.user.avatar!}
+              alt={message.user.username}
+              width={50}
+              height={50}
+              className="size-8 rounded-full"
+            />
+          )}
+          <div
+            className={`flex flex-col gap-1 ${
+              message.userId === userId ? "items-end" : ""
+            }`}
+          >
+            <span
+              className={`${
+                message.userId === userId ? "bg-neutral-500" : "bg-orange-500"
+              } p-2.5 rounded-md`}
+            >
+              {message.payload}
+            </span>
+            <span className="text-xs">
+              {formatToTimeAgo(message.created_at.toString())}
+            </span>
+          </div>
+        </div>
+      ))}
+      <form className="flex relative" onSubmit={onSubmit}>
+        <input
+          required
+          onChange={onChange}
+          value={message}
+          className="bg-transparent rounded-full w-full h-10 focus:outline-none px-5 ring-2 focus:ring-4 transition ring-neutral-200 focus:ring-neutral-50 border-none placeholder:text-neutral-400"
+          type="text"
+          name="message"
+          placeholder="Write a message..."
+        />
+        <button className="absolute right-0">
+          <ArrowUpCircleIcon className="size-10 text-orange-500 transition-colors hover:text-orange-300" />
+        </button>
+      </form>
+    </div>
+  );
+}
