e32a1e8cafe228cd5dab1a402e60649d9e3b6bde
니꼬 <nico@nomadcoders.co>
2024-04-04 19:07:03 +0900

    16.0 Introduction

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,26 @@
 import type { Metadata } from "next";
-import { Roboto, Rubik_Scribble } from "next/font/google";
-import localFont from "next/font/local";
+// import { Roboto, Rubik_Scribble } from "next/font/google";
+// import localFont from "next/font/local";
 import "./globals.css";
 
-const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["400", "500"],
-  style: ["normal"],
-  variable: "--roboto-text",
-});
+// const roboto = Roboto({
+//   subsets: ["latin"],
+//   weight: ["400", "500"],
+//   style: ["normal"],
+//   variable: "--roboto-text",
+// });
 
-const rubick = Rubik_Scribble({
-  weight: "400",
-  style: "normal",
-  subsets: ["latin"],
-  variable: "--rubick-text",
-});
+// const rubick = Rubik_Scribble({
+//   weight: "400",
+//   style: "normal",
+//   subsets: ["latin"],
+//   variable: "--rubick-text",
+// });
 
-const metallica = localFont({
-  src: "./metallica.ttf",
-  variable: "--metallica-text",
-});
+// const metallica = localFont({
+//   src: "./metallica.ttf",
+//   variable: "--metallica-text",
+// });
 
 export const metadata: Metadata = {
   title: {
@@ -37,9 +37,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={` ${roboto.variable} ${rubick.variable} ${metallica.variable} bg-neutral-900 text-white max-w-screen-sm mx-auto`}
-      >
+      <body className={` bg-neutral-900 text-white max-w-screen-sm mx-auto`}>
         {children}
       </body>
     </html>
